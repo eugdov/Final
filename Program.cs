b3b76fa18af144650a14fa4f11af557001e0de93
eugdov <eugene.dov@gmail.com>
2022-01-27 21:15:28 +0300

    Решение задачи

--- Program.cs
+++ Program.cs
@@ -1,1 +1,36 @@
-﻿
+﻿string[] FirstArray = { "Phone", "-667", "Ж", "просто какой-то текст", "GeekBrains", "Bobby", "1&!", "ffffaaf", "ХЪ" };
+int n = 0;
+
+for (int x = 0; FirstArray.Length > x; x++)
+{
+    if (FirstArray[x].Length <= 3)
+    {
+        n = n+1;
+    }
+}
+
+string[] SecondArray = new string[n];
+
+int i = 0;
+
+for (int x = 0; FirstArray.Length > x; x++)
+{
+    if (FirstArray[x].Length <= 3)
+    {
+        SecondArray[i] = FirstArray[x];
+        i++;
+    }
+}
+
+void PrintArray(string[] array)
+{
+    int count = array.Length;
+    for (int j = 0; j < count; j++)
+    {
+        Console.Write($"{array[j]}" + "  ");
+    }
+    Console.WriteLine();
+}
+PrintArray(SecondArray);
+
+
